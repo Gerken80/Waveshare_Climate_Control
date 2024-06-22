66377c2f1137e1c8d65df82980b129b63b97103b
ClanHost Australia <38305756+ClanHost@users.noreply.github.com>
2024-06-22 16:41:04 +1000

    Create Wroom_Master.ino

---
+++ Wroom_Master.ino
@@ -0,0 +1,68 @@
+#include "Wire.h"
+#define I2C_DEV_ADDR 0x55
+#define address 0x40
+char dtaUart[15];
+char dtaLen = 0;
+uint8_t Data[100] = {0};
+uint8_t buff[100] = {0};
+uint8_t tInToSend = 46;
+uint8_t tOutToSend = -12;
+uint8_t setTempReceived = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+}
+uint8_t buf[4] = {0};
+uint16_t data, data1;
+float temp;
+float hum;
+void loop()
+{
+  readReg(0x00, buf, 4);
+  data = buf[0] << 8 | buf[1];
+  data1 = buf[2] << 8 | buf[3];
+  temp = ((float)data * 165 / 65535.0) - 40.0;
+  hum =  ((float)data1 / 65535.0) * 100;
+  Serial.print("temp(C):");
+  Serial.print(temp);
+  Serial.print("\t");
+  Serial.print("hum(%RH):");
+  Serial.println(hum);
+  delay(500);
+///////////
+  //Write message to the slave  
+  Wire.beginTransmission(I2C_DEV_ADDR);
+  Wire.write(temp);
+  Wire.write(tOutToSend);
+  uint8_t error = Wire.endTransmission(true);
+
+  //Read 8 bytes from the slave
+  setTempReceived = 0;
+  uint8_t bytesReceived = Wire.requestFrom(I2C_DEV_ADDR, 8);
+
+  if ((bool)bytesReceived) {  
+    setTempReceived = Wire.read(); 
+    Serial.println(setTempReceived);
+  }
+///////////////
+}
+uint8_t readReg(uint8_t reg, const void* pBuf, size_t size)
+{
+  if (pBuf == NULL) {
+    Serial.println("pBuf ERROR!! : null pointer");
+  }
+  uint8_t * _pBuf = (uint8_t *)pBuf;
+  Wire.beginTransmission(address);
+  Wire.write(&reg, 1);
+  if ( Wire.endTransmission() != 0) {
+    return 0;
+  }
+  delay(20);
+  Wire.requestFrom(address, (uint8_t) size);
+  for (uint16_t i = 0; i < size; i++) {
+    _pBuf[i] = Wire.read();
+  }
+  return size;
+}
